8238dd44dd70616e5ff496c01203639b714acfb8
Malcolm Carr <themalcolmcarr@gmail.com>
2019-01-03 19:54:12 -0600

    add cloud functions for event activity

--- src/views/EventDetailView/EventDetailChat.js
+++ src/views/EventDetailView/EventDetailChat.js
@@ -37,7 +37,7 @@ class EventDetailChat extends Component {
           <Comment.Group>
             {eventChat &&
               eventChat.map(comment => (
-                <Comment>
+                <Comment key={comment.id}>
                   <Comment.Avatar
                     src={comment.photoURL || '/assets/user.png'}
                   />
@@ -71,45 +71,49 @@ class EventDetailChat extends Component {
                     </Comment.Actions>
                   </Comment.Content>
 
-               {comment.childNodes && comment.childNodes.map(child => ( <Comment.Group>
-                    <Comment>
-                      <Comment.Avatar
-                        src={child.photoURL || '/assets/user.png'}
-                      />
-                      <Comment.Content>
-                        <Comment.Author
-                          as={Link}
-                          to={`/profile/${child.uid}`}
-                        >
-                          {child.displayName}
-                        </Comment.Author>
-                        <Comment.Metadata>
-                          <div>
-                            {child.date &&
-                              distanceInWords(child.date, Date.now())}{' '}
-                            ago
-                          </div>
-                        </Comment.Metadata>
-                        <Comment.Text>{child.text}</Comment.Text>
-                        <Comment.Actions>
-                          <Comment.Action
-                            onClick={this.onOpenReplyForm(child.id)}
-                          >
-                            Reply
-                          </Comment.Action>
-                          {showReplyForm && currentCommentId === child.id && (
-                            <EventDetailChatForm
-                              addEventComment={addEventComment}
-                              eventId={eventId}
-                              form={`reply_${child.id}`}
-                              closeForm={this.handleCloseReplyForm}
-                              parentId={child.parentId}
-                            />
-                          )}
-                        </Comment.Actions>
-                      </Comment.Content>
-                    </Comment>
-               </Comment.Group>))}
+                  {comment.childNodes &&
+                    comment.childNodes.map(child => (
+                      <Comment.Group key={child.id}>
+                        <Comment>
+                          <Comment.Avatar
+                            src={child.photoURL || '/assets/user.png'}
+                          />
+                          <Comment.Content>
+                            <Comment.Author
+                              as={Link}
+                              to={`/profile/${child.uid}`}
+                            >
+                              {child.displayName}
+                            </Comment.Author>
+                            <Comment.Metadata>
+                              <div>
+                                {child.date &&
+                                  distanceInWords(child.date, Date.now())}{' '}
+                                ago
+                              </div>
+                            </Comment.Metadata>
+                            <Comment.Text>{child.text}</Comment.Text>
+                            <Comment.Actions>
+                              <Comment.Action
+                                onClick={this.onOpenReplyForm(child.id)}
+                              >
+                                Reply
+                              </Comment.Action>
+                              {showReplyForm &&
+                                currentCommentId === child.id && (
+                                  <EventDetailChatForm
+                                    addEventComment={addEventComment}
+                                    eventId={eventId}
+                                    form={`reply_${child.id}`}
+                                    closeForm={this.handleCloseReplyForm}
+                                    parentId={child.parentId}
+                                  />
+                                )}
+                            </Comment.Actions>
+                          </Comment.Content>
+                        </Comment>
+                      </Comment.Group>
+                    ))}
                 </Comment>
               ))}
           </Comment.Group>
